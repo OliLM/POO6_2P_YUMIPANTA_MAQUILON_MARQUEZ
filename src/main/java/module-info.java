52427319461e5548926c074e361cd28de1fea55d
Olivier Marquez <olirmarq@espol.edu.ec>
2022-01-27 22:44:24 -0500

    Borrando proyecto defectuoso

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.poo.proyectopoo2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens com.poo.proyectopoo2 to javafx.fxml;
-    exports controladores;
-}

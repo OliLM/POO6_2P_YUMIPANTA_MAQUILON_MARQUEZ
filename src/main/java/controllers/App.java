7d8a166e7a75be7642a181979eb1a77e9f636f60
unknown <ismeyumi@espol.edu.ec>
2022-01-29 15:23:35 -0500

    Creacion de metodovalidar usuario

--- src/main/java/controllers/App.java
+++ src/main/java/controllers/App.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.proyecto2p;
+package controllers;
 
 import java.io.IOException;
 import javafx.application.Application;
@@ -18,7 +18,7 @@ import javafx.stage.Stage;
  */
 public class App extends Application{
     private static Scene scene;
-    public static String imagen="src/main/resources/imagenes/fondo.jpg";
+    public static String imagen="src/main/resources/imagenes/";
     public static String pathFiles="src/main/resources/files/";
     
     @Override
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,5 @@ module com.mycompany.proyecto2p {
     requires java.base;
 
     opens com.mycompany.proyecto2p to javafx.fxml;
-    exports com.mycompany.proyecto2p;
+    exports controllers;
 }

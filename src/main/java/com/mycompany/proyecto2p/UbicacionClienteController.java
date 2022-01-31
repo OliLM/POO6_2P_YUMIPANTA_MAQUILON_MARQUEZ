b154920e6253583382ef7288d2ed26248356f709
william <wmaquilo@espol.edu.ec>
2022-01-30 21:54:38 -0500

    proyecto Actualizado

--- src/main/java/com/mycompany/proyecto2p/UbicacionClienteController.java
+++ src/main/java/com/mycompany/proyecto2p/UbicacionClienteController.java
@@ -9,7 +9,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -18,6 +20,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
@@ -26,6 +30,10 @@ import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import clasesModelos.CamposIncompletosException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import javafx.scene.control.Label;
 
 /**
  * FXML Controller class
@@ -46,6 +54,8 @@ public class UbicacionClienteController implements Initializable {
     private TextField textHora;
     @FXML
     private Button btnFinalizar;
+    @FXML
+    private Label mensajeAviso;
 
     /**
      * Initializes the controller class.
@@ -53,23 +63,54 @@ public class UbicacionClienteController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        
-        try(FileInputStream input =new FileInputStream(App.imagen+"ubicacion.png")){
-            Image i=new Image(input);
-            BackgroundImage bgI=new BackgroundImage(i, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT); 
-            Background bg= new Background(bgI);
+
+        try (FileInputStream input = new FileInputStream(App.imagen + "ubicacion.png")) {
+            Image i = new Image(input);
+            BackgroundImage bgI = new BackgroundImage(i, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+            Background bg = new Background(bgI);
             pane.setBackground(bg);
-        }catch(Exception i){
+        } catch (Exception i) {
             System.out.println("algo salio mal ya fue");
-            
+
         }
-        
-    }    
+
+        pane.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent e) {
+
+//                
+                textDireccion.clear();
+                textHora.clear();
+                textFecha.clear();
+                try {
+                    if (false) {
+                        
+                        mensajeAviso.setText("");
+                    } else {
+                        throw new CamposIncompletosException();
+                    }
+
+                } catch (CamposIncompletosException exc) {
+                    mensajeAviso.setText("Campos incompletos, no se puede agregar");
+                }
+
+                try (BufferedWriter bf = new BufferedWriter(new FileWriter(App.pathFiles + "contratacionesPruebas.txt"))) {
+
+                } catch (IOException ex) {
+                    
+
+                }
+
+            }
+
+        });
+
+    }
 
     @FXML
     private void guardarInformacion(ActionEvent event) {
-        
 
     }
-    
+
 }
